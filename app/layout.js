94f8e765ebae649eede8f797ff49a86f3211df3b
Moshe Hayoun <yelotag@gmail.com>
2026-05-07 17:18:30 +0300

    fix gemini and one error Metadata: Cloud fetch failed, falling back to local file Error: Dynamic server usage: Route / couldn't be rendered statically because it used revalidate: 0 fetch /api/settings /. See more info here: https://nextjs.org/docs/messages/dynamic-server-error

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import ClientProviders from "@/components/ClientProviders"; // Importez le nouveau fichier
 
+export const dynamic = "force-dynamic";
+
 const geistSans = Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
